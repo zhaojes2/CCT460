d8e15371b1d1b7da61e22778d322ed23670d8ad1
zhaojes2 <jess.zyb@gmail.com>
2015-07-08 14:55:48 -0400

    1

---
+++ superfab.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<title> Superfab Office Products</title> 
+	<link rel="stylesheet" type="text/css" href="style.css"> 
+</head> 
+
+<body> 
+
+<div class="header">
+	<div class="container">
+		<h1> Superfab Office Products </h1>
+		<h2> Office Supply Products Division </h2>
+	</div>
+</div> 
+
+<div class="menu">
+	<div class="container">
+		<ul class="menulist">
+			<li> <a href="https://phoenix.sheridanc.on.ca/~ccit2724/homework/superfab.php"> Main Page </a> </li>
+			<li> <a href="https://phoenix.sheridanc.on.ca/~ccit2724/homework/contacts.php"> Contact Us </a> </li>
+			
+		</ul>
+	</div>
+</div>
+
+<div class="content">
+	<div class="container">
+
+	<img src="http://www.codediva.com/cct460/summer2015/superfabceo.jpg" align="right"/>
+
+	<p> The Superfab name has come to mean quality and innovation in advanced office solutions. Never mind mundane office offerings for back-room pencil-pushing geeks ... Superfab offerings are smart, slick, and so far ahead of its time that it's well worth the outrageous prices we demand for them. </p>
+
+	<p> Our award-winning products include: MegalaMartTM, PATTM, Hinternet HunterTM, DAISYTM, and BugSmasher SweetTM, plus other amazing devices that are so secret, if we told you about them we'd then have to swear you to secrecy and degauss your hard drive.</p>
+	
+	<p> The biggest corporations all over the world practically flock to our door to have their most ferocious technological challenges turned into purring pussycats by the boy brainiacs and girl geniuses on our staff.</p>
+	
+	<p> The Superfab laboratory is located in Sasquatch Falls, Washington (the state whose rich negative ion levels (spawned by high annual rainfall levels) and scenic ocean views seem to inspire technogeeks and propeller-heads to awesome levels of coding productivity ... not that their cubicles actually have a window, or view of any kind). </p>
+	
+	<p> The lab is presided over by the legendary Dr. Murry Duchance, PhD, winner of the 1995 Nobel Prize in Physics, 1996 National Science Foundation gold medal, 1997 Thomas Alva Edison Prize, and the 1998 Grammy Award for best new rap album.</p>
+	
+	<p> But Dr. Duchance is only one of our many brainiacs.  With so many unmitigated geniuses on the payroll, often stumbling over one another, Superfab likes to get them out of the office from time to time (otherwise they might forget how to tie their shoelaces, and perform other simple social functions that the rest of us take for granted).  Ask us about of in-company supply program!  We'll develop a custom program to your specifications, or simply show up as consultants and dazzle you with our brilliant thinking, and no doubt leave you depressed by your own stupidity.</p>
+	</div>
+</div>
+
+<div class="footer">	
+	<div class="container">
+	<?php 
+	echo "Name: ". "Yi Bing (Jessica) Zhao  ";
+	echo "Student Number: ". "999826210  ". "<br>";
+	echo "Today is " . date(" l "). date("Y/m/d."); ?>
+	</div>
+</div>
+
+</body>
+</html>
