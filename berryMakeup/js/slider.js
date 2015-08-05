c666ecfee5ba67c96df1038b73be848ecd26b6b3
tifflam <tiffany.lam@mail.utoronto.ca>
2015-08-04 21:44:56 -0400

    updated reponsive

--- berryMakeup/js/slider.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function(jQuery) {
-    jQuery("#gallery li img").hover(function(){
-        jQuery('#main-img').attr('src',jQuery(this).attr('src').replace('thumb/', '')).parent().attr('href',jQuery(this).parent().attr('href'));
-    });
-});
